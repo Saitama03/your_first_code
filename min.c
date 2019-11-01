fadeb6d6de98a14d6543c894659c53e48cbcf799
Saitama03 <57271725+Saitama03@users.noreply.github.com>
2019-11-01 20:59:54 +0000

    Update min.c
    hellooo

--- min.c
+++ min.c
@@ -1,7 +1,6 @@
 int main()
 {
-  int i;
-  for (i = 1; i < 32; i++)
-    printf("Hello Holberton\n");
-  return 0;
-}
+    int i;
+    for(i=1;i<=10;printf("  Hello World ! \n",i++));
+    return 0;
+} 
